e2a75626a0f3c48cd0c20fb988243588ef61d520
anatawa12 <anatawa12@icloud.com>
2022-02-23 15:36:12 +0900

    add seemense to whitelist

--- image/whitelist.json.sh
+++ image/whitelist.json.sh
@@ -16,6 +16,7 @@ add_user 6cde5c61-9493-4726-b580-a74e751e5eb4 anatawa12
 add_user f060a0d5-a246-43fb-8c92-2395798483da kirisamekuroa
 add_user 0ee944a1-4a15-488c-9ef0-8edae0bfd128 Alice1537
 add_user 52806416-f227-49ef-8fd8-0fca389c793a kaka_256
+add_user 87229700-66c8-4436-8cbc-f288b7bae8ec seemense
 
 echo "[
 $JSON_BODY
