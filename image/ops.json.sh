36433d540ef0488798bee74068c42fd8b182dced
anatawa12 <anatawa12@icloud.com>
2022-02-23 15:38:42 +0900

    add subaru1572 to operators

--- image/ops.json.sh
+++ image/ops.json.sh
@@ -14,6 +14,7 @@ add_user() {
 
 add_user 6cde5c61-9493-4726-b580-a74e751e5eb4 4 anatawa12 true
 add_user 0ee944a1-4a15-488c-9ef0-8edae0bfd128 3 Alice1537 true
+add_user 6682c0b9-3ce3-4cae-84b4-e6ffc42e664d 3 subaru1572 true
 
 echo "[
 $JSON_BODY
