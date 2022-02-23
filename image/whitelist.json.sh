ff01bdb4c84a1371a99f760f80012ab6ba1cd4a7
anatawa12 <anatawa12@icloud.com>
2022-02-23 15:35:43 +0900

    add kaka_256 to whitelist

--- image/whitelist.json.sh
+++ image/whitelist.json.sh
@@ -15,6 +15,7 @@ add_user() {
 add_user 6cde5c61-9493-4726-b580-a74e751e5eb4 anatawa12
 add_user f060a0d5-a246-43fb-8c92-2395798483da kirisamekuroa
 add_user 0ee944a1-4a15-488c-9ef0-8edae0bfd128 Alice1537
+add_user 52806416-f227-49ef-8fd8-0fca389c793a kaka_256
 
 echo "[
 $JSON_BODY
